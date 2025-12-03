84c53d0cf94112e276b70c485a9cd16ff8e2cd70
abhidhamma-private <abhidhamma91@gmail.com>
2025-12-04 06:28:49 +0900

    배포404수정2

--- api/index.rs
+++ api/index.rs
@@ -1,33 +1,25 @@
 use axum::{
-    routing::{get, get_service, post},
+    routing::{get, post},
     Router,
 };
-use std::net::SocketAddr;
-
 use juggernaut_calculator::handlers::{
     calculate_and_save_amrap, get_program, get_user, upsert_user,
 };
+use vercel_runtime::{run, Body, Error, Request, Response};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // .env 파일에서 환경 변수 로드
     dotenvy::dotenv().ok();
 
-    // API 라우터를 먼저 정의
-    let api_router = Router::new()
+    let app = Router::new()
         .route("/api/user/:username", get(get_user).post(upsert_user)) // API 라우트
         .route("/api/program/:username", get(get_program))
         .route("/api/amrap/:username", post(calculate_and_save_amrap));
 
-    // Vercel 라우팅 규칙에 따라 API 라우터만 정의
-    let app = api_router;
-
-    // 서버 실행
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("서버 실행 중: http://{}", addr);
-
-    let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, app).await?;
-
+    run(app).await?;
     Ok(())
+}
+
+async fn handler(req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder().status(200).body(Body::Empty)?)
 }
